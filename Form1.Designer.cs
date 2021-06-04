cf77fc15a63eaba68e229b6fc6d6286c54221da1
matiwa <mateuszivan1994@wp.pl>
2021-06-04 14:56:51 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Bacon_cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gBSource = new System.Windows.Forms.GroupBox();
+            this.rBText = new System.Windows.Forms.RadioButton();
+            this.rBFile = new System.Windows.Forms.RadioButton();
+            this.gBOperation = new System.Windows.Forms.GroupBox();
+            this.rBD = new System.Windows.Forms.RadioButton();
+            this.rBE = new System.Windows.Forms.RadioButton();
+            this.btngo = new System.Windows.Forms.Button();
+            this.tBText = new System.Windows.Forms.TextBox();
+            this.tBMessage = new System.Windows.Forms.TextBox();
+            this.gBSource.SuspendLayout();
+            this.gBOperation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gBSource
+            // 
+            this.gBSource.Controls.Add(this.rBFile);
+            this.gBSource.Controls.Add(this.rBText);
+            this.gBSource.Location = new System.Drawing.Point(13, 13);
+            this.gBSource.Name = "gBSource";
+            this.gBSource.Size = new System.Drawing.Size(69, 73);
+            this.gBSource.TabIndex = 0;
+            this.gBSource.TabStop = false;
+            this.gBSource.Text = "gBSource";
+            // 
+            // rBText
+            // 
+            this.rBText.AutoSize = true;
+            this.rBText.Checked = true;
+            this.rBText.Location = new System.Drawing.Point(7, 20);
+            this.rBText.Name = "rBText";
+            this.rBText.Size = new System.Drawing.Size(46, 17);
+            this.rBText.TabIndex = 0;
+            this.rBText.TabStop = true;
+            this.rBText.Text = "Text";
+            this.rBText.UseVisualStyleBackColor = true;
+            this.rBText.CheckedChanged += new System.EventHandler(this.SelectSource);
+            // 
+            // rBFile
+            // 
+            this.rBFile.AutoSize = true;
+            this.rBFile.Location = new System.Drawing.Point(7, 43);
+            this.rBFile.Name = "rBFile";
+            this.rBFile.Size = new System.Drawing.Size(41, 17);
+            this.rBFile.TabIndex = 1;
+            this.rBFile.Text = "File";
+            this.rBFile.UseVisualStyleBackColor = true;
+            this.rBFile.CheckedChanged += new System.EventHandler(this.SelectSource);
+            // 
+            // gBOperation
+            // 
+            this.gBOperation.Controls.Add(this.rBD);
+            this.gBOperation.Controls.Add(this.rBE);
+            this.gBOperation.Location = new System.Drawing.Point(12, 92);
+            this.gBOperation.Name = "gBOperation";
+            this.gBOperation.Size = new System.Drawing.Size(78, 73);
+            this.gBOperation.TabIndex = 2;
+            this.gBOperation.TabStop = false;
+            this.gBOperation.Text = "Operations";
+            // 
+            // rBD
+            // 
+            this.rBD.AutoSize = true;
+            this.rBD.Location = new System.Drawing.Point(7, 43);
+            this.rBD.Name = "rBD";
+            this.rBD.Size = new System.Drawing.Size(62, 17);
+            this.rBD.TabIndex = 1;
+            this.rBD.Text = "Decrypt";
+            this.rBD.UseVisualStyleBackColor = true;
+            this.rBD.CheckedChanged += new System.EventHandler(this.Operation);
+            // 
+            // rBE
+            // 
+            this.rBE.AutoSize = true;
+            this.rBE.Checked = true;
+            this.rBE.Location = new System.Drawing.Point(7, 20);
+            this.rBE.Name = "rBE";
+            this.rBE.Size = new System.Drawing.Size(61, 17);
+            this.rBE.TabIndex = 0;
+            this.rBE.TabStop = true;
+            this.rBE.Text = "Encrypt";
+            this.rBE.UseVisualStyleBackColor = true;
+            this.rBE.CheckedChanged += new System.EventHandler(this.Operation);
+            // 
+            // btngo
+            // 
+            this.btngo.Location = new System.Drawing.Point(12, 172);
+            this.btngo.Name = "btngo";
+            this.btngo.Size = new System.Drawing.Size(75, 23);
+            this.btngo.TabIndex = 3;
+            this.btngo.Text = "Encrypt";
+            this.btngo.UseVisualStyleBackColor = true;
+            this.btngo.Click += new System.EventHandler(this.btngo_Click);
+            // 
+            // tBText
+            // 
+            this.tBText.Location = new System.Drawing.Point(98, 13);
+            this.tBText.Multiline = true;
+            this.tBText.Name = "tBText";
+            this.tBText.Size = new System.Drawing.Size(344, 289);
+            this.tBText.TabIndex = 4;
+            // 
+            // tBMessage
+            // 
+            this.tBMessage.Enabled = false;
+            this.tBMessage.Location = new System.Drawing.Point(448, 13);
+            this.tBMessage.Multiline = true;
+            this.tBMessage.Name = "tBMessage";
+            this.tBMessage.Size = new System.Drawing.Size(340, 289);
+            this.tBMessage.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 314);
+            this.Controls.Add(this.tBMessage);
+            this.Controls.Add(this.tBText);
+            this.Controls.Add(this.btngo);
+            this.Controls.Add(this.gBOperation);
+            this.Controls.Add(this.gBSource);
+            this.Name = "Form1";
+            this.Text = "Bacon cipher";
+            this.gBSource.ResumeLayout(false);
+            this.gBSource.PerformLayout();
+            this.gBOperation.ResumeLayout(false);
+            this.gBOperation.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gBSource;
+        private System.Windows.Forms.RadioButton rBFile;
+        private System.Windows.Forms.RadioButton rBText;
+        private System.Windows.Forms.GroupBox gBOperation;
+        private System.Windows.Forms.RadioButton rBD;
+        private System.Windows.Forms.RadioButton rBE;
+        private System.Windows.Forms.Button btngo;
+        private System.Windows.Forms.TextBox tBText;
+        private System.Windows.Forms.TextBox tBMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Bacon_cipher
+{
+    public partial class Form1 : Form
+    {
+        FileStream file = new FileStream("message.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        char[] chars = new char[]{' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0',
+            '1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','Ą','B','C','Ć','D','E',
+            'Ę','F','G','H','I','J','K','L','Ł','M','N','Ń','O','Ó','P','Q','R','S','Ś','T','U','V','W',
+            'X','Y','Z','Ź','Ż','[','/',']','^','_','`','a','ą','b','c','ć','d','e','ę','f','g','h','i',
+            'j','k','l','ł','m','n','ń','o','ó','p','q','r','s','ś','t','u','v','w','x','y','z','ź','ż',
+            '{','|','}','~'}; //ASCII code combined with Polish diacritized letters
+        string[] bacon;//Bacon ciphertext declaration
+        string message = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            bacon = new string[chars.Length];
+            //generating a ciphertext table according to Bacon's scheme
+            for (int i = 0; i < chars.Length; i++)
+            {
+                string b = "";
+                int j = i;
+                while (j > 0)
+                {
+                    if(j%2==0) b = "a"+b;
+                    else if (j % 2 == 1) b = "b" + b;
+                    j /= 2;
+                }
+                while (b.Length < 7) b = "a" + b;
+                bacon[i] = b;
+            }
+        }
+
+        //for decryption, the algorithm does not work if the message length is not divided by seven!!!
+        private void btngo_Click(object sender, EventArgs e)
+        {
+            //loading the message from the text field and saving it to a file
+            if (rBText.Checked == true)
+            {
+                message = tBText.Text;
+                StreamWriter sw = new StreamWriter(file);
+                sw.WriteLine(message);
+                sw.Close();
+            }
+            //loading a message from a file and saving it to the text field
+            else if (rBFile.Checked == true)
+            {
+                StreamReader sr = new StreamReader(file);
+                message = sr.ReadLine();
+                sr.Close();
+                tBText.Text = message;
+            }
+            //message encryption
+            if (rBE.Checked == true)
+            {
+                char[] messagechar = message.ToCharArray();
+                for (int i = 0; i < messagechar.Length; i++)
+                {
+                    for(int j = 0; j < chars.Length; j++)
+                    {
+                        if (messagechar[i] == chars[j]) tBMessage.Text += bacon[j];
+                    }
+                }
+            }
+            //message decryption (each encrypted character has 7 bits)
+            else if (rBD.Checked == true)
+            {
+                char[] messagechar = message.ToCharArray();
+                string bufor = "";
+                for (int i = 0; i < messagechar.Length; i++)
+                {
+                    bufor += messagechar[i].ToString();
+                    if (i % 7 == 6)
+                    //each encrypted character has 7 bits, so I check the character against the Bacon table
+                    {
+                        for (int j = 0; j < bacon.Length; j++)
+                        {
+                            if (bufor == bacon[j]) tBMessage.Text += chars[j].ToString();
+                        }
+                        bufor = "";
+                    }                    
+                }
+            }
+            //protection against the next operation
+            gBOperation.Enabled = false;
+            gBSource.Enabled = false;
+            btngo.Enabled = false;
+            tBText.Enabled = false;
+        }
+
+        //display the selected operation as the button text
+        private void Operation(object sender, EventArgs e)
+        {
+            if (rBE.Checked == true) btngo.Text = rBE.Text;
+            else if (rBD.Checked == true) btngo.Text = rBD.Text;
+        }
+
+        //select a message input method
+        private void SelectSource(object sender, EventArgs e)
+        {
+            if (rBText.Checked == true) tBText.Enabled = true;
+            else if (rBFile.Checked == true) tBText.Enabled = false;
+        }
+    }
+}
